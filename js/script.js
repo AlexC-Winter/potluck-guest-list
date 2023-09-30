e2769b98a1c98f9748a8a04ffaa11d3eca395a96
Alex_W <alexcwinter7@gmail.com>
2023-09-30 18:19:02 -0500

    added guest limit of 8

--- js/script.js
+++ js/script.js
@@ -27,6 +27,19 @@ addGuestButton.addEventListener("click", function(){
     if (guest !== "") {
         addToList(guest)
         clearInput()
+        updateGuestCount()
     }
 })
+
+const updateGuestCount = function(){
+    const guests = document.querySelectorAll(".guest-list li")
+    guestCount.innerText = guests.length
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide")
+        guestInput.classList.add("hide")
+        guestInputLabel.classList.add("hide")
+        guestFull.classList.remove("hide")
+    }
+}
+
 
